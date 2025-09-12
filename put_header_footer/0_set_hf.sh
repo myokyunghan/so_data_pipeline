5086aa5cdf8e2b69511cceabdf75d8fd3757252e
boysbeanxious <ccunyday@naver.com>
2025-09-12 13:51:34 +0900

    20250912

--- put_header_footer/0_set_hf.sh
+++ put_header_footer/0_set_hf.sh
@@ -1,28 +1,28 @@
 #!/bin/zsh
 
-files=$(find ../divide_comment -name "*.xml") 
+path=$1
+type=$2
 
-for file in ../divide_comment/*.xml
- do
-	type="comments"
-	echo $type
-	
-	head=`sed -n '1p' $file`
-	tail=`sed -n '$p' $file`
+echo $path
+echo $type
+
+for file in $path/*.xml
+do	
+	#echo $file
+	head=`/usr/bin/sed -n '1p' $file`
+	tail=`/usr/bin/sed -n '$p' $file`
 	
 	head_tag="<"${type}">"
-	tail_tag="</"${type}">"
+	footer_tag="</"${type}">"
 	echo $head_tag > head_tag.sh
+	echo $footer_tag > footer.sh
 	
-	echo $file_nm
-	echo " "			
-
 	if [[ $head = *"xml"* ]]; then
 		echo ""
 	else
 		echo "no header!!!"
-		perl -p -i -e '$.==1 and print `cat head_tag.sh`' `echo $file`
-		perl -p -i -e '$.==1 and print `cat header.sh`' `echo $file`
+		/usr/bin/perl -p -i -e '$.==1 and print `cat head_tag.sh`' `echo $file`
+		/usr/bin/perl -p -i -e '$.==1 and print `cat header.sh`' `echo $file`
 	fi
 
 	if [[ $tail = *"$type"* ]]; then
